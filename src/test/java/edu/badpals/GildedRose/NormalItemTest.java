69aab0ca27d90cbbdafc34a83304220c2211f23e
Victor <Victorsancheznogueira@gmail.com>
2024-05-02 15:24:45 +0200

    fix: quality test to param

--- src/test/java/edu/badpals/GildedRose/NormalItemTest.java
+++ src/test/java/edu/badpals/GildedRose/NormalItemTest.java
@@ -3,43 +3,35 @@ package edu.badpals.GildedRose;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class NormalItemTest {
-    static NormalItem item = new NormalItem("Dexterity Vest", 0, 3);
-    @Test
-    public void qualityAndSellInPositiveTest(){
+    @ParameterizedTest
+    @CsvSource({
+            "0,  3, 2",
+            "-1,  3, 1",
+            "0,  0, 0",
+            "-1,  1, 0"
+    })
+    public void qualityTest(int sellIn, int quality, int resoult){
+        NormalItem item = new NormalItem("Dexterity Vest", sellIn, quality);
         item.updateQuality();
-        assertEquals(2, item.getQuality()); //Actual Item quality = 2, sellIn = 0
+        assertEquals(resoult, item.getQuality());
     }
     @Test
     public void updateSellInTest(){
+        NormalItem item = new NormalItem("Dexterity Vest", 0, 2);
         item.updateSellIn();
         assertEquals(-1, item.getSellIn()); //Actual Item quality = 2, sellIn = -1
-    }
-    @Test
-    public void SellInNegativeTest() {
-        item.updateQuality();
-        assertEquals(0, item.getQuality()); //Actual Item quality = 0, sellIn = -1
-    }
-
-    @Test
-    public void qualityZeroTest() {
-        item.updateQuality();
-        assertEquals(0, item.getQuality()); //Actual Item quality = 0, sellIn = -1
     }
 
     @Test
     public void getNameTest(){
+        NormalItem item = new NormalItem("Dexterity Vest", 0, 0);
         assertEquals("Dexterity Vest", item.getName());
-    }
-    @Test
-
-    public void qualityNotNegativeTest(){
-        NormalItem item2 = new NormalItem("Elixir of the mongoose", -1, 1); //updateQuality does not accept negative quality
-        item2.updateQuality();
-        assertEquals(0, item2.getQuality());
     }
 
 
