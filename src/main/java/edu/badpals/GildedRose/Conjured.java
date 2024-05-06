c7351fa77b482b66b7259eefc96bca0f39d1abc7
SergioPrieto <serprigar@gmail.com>
2024-05-06 09:11:45 +0200

    feat(Conjured.java): add class

--- src/main/java/edu/badpals/GildedRose/Conjured.java
+++ src/main/java/edu/badpals/GildedRose/Conjured.java
@@ -1,23 +1,39 @@
 package edu.badpals.GildedRose;
 
-public class Conjured implements Updateable{
-    @Override
-    public void updateQuality() {
+public class Conjured extends Item implements Updateable{
 
+    public Conjured(String name, int sellIn, int quality) {
+        super(name, sellIn, quality);
     }
 
     @Override
-    public void updateSellIn() {
+    public void updateQuality() {
+        if (sellIn < 0){
+            quality -= 4;
+        } else{
+            quality -= 2;
+        }
+        if (quality < 0){
+            quality  = 0;
+        }
+    }
 
+
+    @Override
+    public void updateSellIn() {
+        sellIn-=1;
     }
 
     @Override
     public int getQuality() {
-        return 0;
+        return quality;
     }
 
     @Override
     public int getSellIn() {
-        return 0;
+        return sellIn;
+    }
+    public String getName() {
+        return this.name;
     }
 }
