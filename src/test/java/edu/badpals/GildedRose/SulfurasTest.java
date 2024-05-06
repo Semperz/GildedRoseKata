4bcd8d44d976b5dbcb332c9cdfdce314ceced107
SergioPrieto <serprigar@gmail.com>
2024-05-06 08:37:54 +0200

    test(SulfurasTest.java): add tests for Sulfuras

---
+++ src/test/java/edu/badpals/GildedRose/SulfurasTest.java
@@ -0,0 +1,35 @@
+package edu.badpals.GildedRose;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SulfurasTest {
+    @ParameterizedTest
+    @CsvSource({
+            "1,  80, 80",
+            "-1,  80, 80",
+            "0,  80, 80"
+    })
+    public void qualityTest(int sellIn, int quality, int resoult){
+        Updateable item = new Sulfuras("Sulfuras", sellIn, quality);
+        item.updateQuality();
+        assertEquals(resoult, item.getQuality());
+    }
+    @Test
+    public void updateSellInTest(){
+        Updateable item = new Sulfuras("Sulfuras", 0, 80);
+        item.updateSellIn();
+        assertEquals(0, item.getSellIn()); //Actual Item quality = 2, sellIn = -1
+    }
+
+    @Test
+    public void getNameTest(){
+        Sulfuras item = new Sulfuras("Sulfuras", 0, 0);
+        assertEquals("Sulfuras", item.getName());
+    }
+
+
+}
