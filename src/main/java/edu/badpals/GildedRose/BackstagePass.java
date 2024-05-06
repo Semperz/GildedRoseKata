9fd2ee4307d20c6102fa2970119f1b9084597560
SergioPrieto <serprigar@gmail.com>
2024-05-06 09:10:55 +0200

    feat(BackstagePass.java): add class

--- src/main/java/edu/badpals/GildedRose/BackstagePass.java
+++ src/main/java/edu/badpals/GildedRose/BackstagePass.java
@@ -1,23 +1,42 @@
 package edu.badpals.GildedRose;
 
-public class BackstagePass implements Updateable{
+public class BackstagePass extends Item implements Updateable {
+    public BackstagePass(String name, int sellIn, int quality) {
+        super(name, sellIn, quality);
+    }
+
     @Override
     public void updateQuality() {
-
+        if (sellIn > 10) {
+            quality += 1;
+        } else if (sellIn > 5) {
+            quality += 2;
+        } else if (sellIn >= 0){
+            quality += 3;
+        } else{
+            quality = 0;
+        }
+        if (quality > 50) {
+            quality = 50;
+        }
     }
 
     @Override
     public void updateSellIn() {
-
+        sellIn -= 1;
     }
 
     @Override
     public int getQuality() {
-        return 0;
+        return quality;
     }
 
     @Override
     public int getSellIn() {
-        return 0;
+        return sellIn;
+    }
+
+    public String getName() {
+        return this.name;
     }
 }
