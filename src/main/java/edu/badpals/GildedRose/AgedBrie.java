0348feb7cc4dc03e5566b548964277093c57d98b
SergioPrieto <serprigar@gmail.com>
2024-05-06 08:51:48 +0200

    test(AgedBrieTest.java): create Aged Brie test

--- src/main/java/edu/badpals/GildedRose/AgedBrie.java
+++ src/main/java/edu/badpals/GildedRose/AgedBrie.java
@@ -1,23 +1,38 @@
 package edu.badpals.GildedRose;
 
-public class AgedBrie implements Updateable{
+public class AgedBrie extends Item implements Updateable{
+
+    public AgedBrie(String name, int sellIn, int quality) {
+        super(name, sellIn, quality);
+    }
+
     @Override
     public void updateQuality() {
-
+        if (sellIn < 0 ){
+            quality += 2;
+        } else{
+            quality += 1;
+        }
+        if (quality > 50){
+            quality = 50;
+        }
     }
 
     @Override
     public void updateSellIn() {
-
+        this.sellIn -= 1;
     }
 
     @Override
     public int getQuality() {
-        return 0;
+        return quality;
     }
 
     @Override
     public int getSellIn() {
-        return 0;
+        return sellIn;
+    }
+    public String getName() {
+        return this.name;
     }
 }
