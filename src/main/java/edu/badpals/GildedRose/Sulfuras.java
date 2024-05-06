349cd6cd7211f88a028152f4c836b2ac59b538c7
SergioPrieto <serprigar@gmail.com>
2024-05-06 08:37:08 +0200

    feat(Sulfuras.java): add class

--- src/main/java/edu/badpals/GildedRose/Sulfuras.java
+++ src/main/java/edu/badpals/GildedRose/Sulfuras.java
@@ -1,24 +1,29 @@
 package edu.badpals.GildedRose;
 
-public class Sulfuras implements Updateable{
+public class Sulfuras extends Item implements Updateable{
+
+    public Sulfuras(String name, int sellIn, int quality) {
+        super(name, sellIn, quality);
+    }
 
     @Override
     public void updateQuality() {
-
     }
 
     @Override
     public void updateSellIn() {
-
     }
 
     @Override
     public int getQuality() {
-        return 0;
+        return this.quality;
     }
 
     @Override
     public int getSellIn() {
-        return 0;
+        return this.sellIn;
+    }
+    public String getName() {
+        return this.name;
     }
 }
